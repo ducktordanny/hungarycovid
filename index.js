cd6183b3b52dc6ac4bc228c204a993abf55d47a8
Daniel Lazar <lazardani0112@gmail.com>
2021-01-18 20:12:52 +0100

    PORT added

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.get('/covid_datas', async (req, res) => {
    }
 });
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
    console.log(`Listening to port ${PORT}`);
 })
