0252be8ad6cd41006d51381bc19ae89843abede6
Daniel Lazar <lazardani0112@gmail.com>
2021-01-19 00:30:36 +0100

    changes

--- test.js
+++ test.js
@@ -1,15 +1,4 @@
-const { text } = require('body-parser');
-const express = require('express');
-const fetch = require('node-fetch');
+require('dotenv').config();
 
-const app = express();
-
-app.use(express.json());
-
-const test = new Promise(async (req, resasd) => {
-   resasd('asdasdasda');
-});
-
-test.then(asd => {
-   console.log(asd.message);
-})
+const date = new Date()
+console.log(date.toLocaleString('en-US', { timeZone: 'Europe/Berlin' }));
