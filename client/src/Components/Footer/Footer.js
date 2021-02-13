a4b829c2928613dec6f11d5f36c2ff4b4c1b3d63
Daniel Lazar <lazardani0112@gmail.com>
2021-02-13 22:51:10 +0100

    LICENSE added and css modifications

--- client/src/Components/Footer/Footer.js
+++ client/src/Components/Footer/Footer.js
@@ -1,9 +1,11 @@
 import React from 'react'
+import ducktorLogo from '../../ducktorD.png';
 import './Footer.css';
 
 const Footer = () => (
    <footer>
-      <label htmlFor="">Made by DucktorDanny <span>{'{'}🦆{'}'}</span></label>
+      <label htmlFor="footer">Made by <a href='https://github.com/ducktorD'>ducktorD</a></label>
+      <img src={ ducktorLogo } alt='ducktorD logo' />
    </footer>
 )
 
